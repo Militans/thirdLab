d3907e6e7840bc81751cc60b73375c1151bd0f2c
Militans <ivangreku48@gmail.com>
2022-12-15 16:12:47 +0300

    firstTry

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+data = open("data.html", 'r', encoding="UTF-8")
+s = ''
+for i in data:
+    s += i
+data.close()
+match = re.findall(r"\b[A-Za-z0-9]+[-_.]*\w+@\w+\.?\w+\.\w+\b", s, re.ASCII)
+print(match)
